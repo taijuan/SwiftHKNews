2e80f1a8b8b8e989974699498a3bb61fb214001e
zhengtaijuan <673636090@qq.com>
2019-07-02 14:13:22 +0800

    init

---
+++ HKNews-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HKNews-Swift
+//
+//  Created by 郑泰捐 on 2019/7/2.
+//  Copyright © 2019 郑泰捐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# HKNews-Swift
