67a06c68e8e5db49ef5d2933fb5f8b45e0d66b29
zhengtaijuan <673636090@qq.com>
2019-07-22 16:51:25 +0800

    delete Pods

--- Pods/Target Support Files/DNSPageView/DNSPageView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DNSPageView : NSObject
-@end
-@implementation PodsDummy_DNSPageView
-@end
--- Pods/Target Support Files/Pods-HKNews-Swift/Pods-HKNews-Swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HKNews_Swift : NSObject
-@end
-@implementation PodsDummy_Pods_HKNews_Swift
-@end
